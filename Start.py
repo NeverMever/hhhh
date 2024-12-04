44a8fc2abd906791ced54e335f3a4fb4a9d3c574
NeverMever <mikhaylova00164@gamil.com>
2024-12-04 13:25:43 +0800

    init

--- Start.py
+++ Start.py
@@ -1,65 +1,41 @@
-import pygame
-import random
+import cv2
+from ultralytics import YOLO
+import numpy as np 
 
-screen_width = screen_height = 800
+def load_yolo_model(model_path):
+    return YOLO(model_path)
 
-pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height))
-done = False
+def detect_objects(model, frame, class_names, display_width):
+    resized_frame = cv2.resize(frame, (display_width, int(display_width / frame.shape[1] *frame.shape[0])))
 
-number_of_stars = 100
-speed = 0.1
-stars = [] # К списку не нужны ключи и, в отличии от кортежа его можно изменять
+    results = model(resized_frame, stream = True) 
 
-def new_star() -> list:
-    star = [random.randint(-screen_width // 2, screen_width // 2),
-            random.randint(-screen_height // 2, screen_height // 2), 256, 10]
-    return star
+    for r in results:
+        if r.masks:
+            masks = r.marks.data.cpu().numpy()
 
-for i in range(0, number_of_stars):
-    stars.append(new_star())
+        for i, mask in enumerate(masks):
+            mask_resized = cv2.resize(mask, (resized_frame.shpae[1], resized_frame.shape[0]), interpolation=cv2.INTER_NEAREST)  
 
-star_colors = [(random.randint(0, 255), random.randint(0, 255),
-                random.randint(0, 255)) for _ in range(number_of_stars)] # создаёт список кортежей из трёх чисел в коллечестве number_of_stars
-color_change_time = 1  # Частота изменения цвета в секундах
-last_color_change = pygame.time.get_ticks() # Присваивается текущее время в миллисекундах с помощью `pygame.time.get_ticks()`
+            overlay = np.zeros_like(resized_frame, dtype=np.uint8)
+            overlay[mask_resized.astype(bool)] = [225, 0, 225]
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+            resized_frame = cv2.addWeighted(resized_frame, 0.8, overlay, 0.2, 0)
 
-    screen.fill((0, 0, 0))
-    current_time = pygame.time.get_ticks()
+    if r.boxes:
+        for box in r.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0]) 
+            conf = round(box.conf[0].item(), 2)
+            cls = int(box,cls[0])
+            label = f"{class_names[cls]}{conf}"
 
-    for i in range(0, number_of_stars):
-        s = stars[i]
+            cv2.rectangle(resized_frame, (x1, y1), (x2, y2), (0, 225, 0), 5)
 
-        # x = s[0]
-        # y = s[1]
-        x = s[0] * 256 / s[2]
-        y = s[1] * 256 / s[2]
-        s[2] -= speed
+            cv2.putText(resized_frame, label, (x1, max(y1-10, 0)), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0,225, 0), 3)
 
-        if x < -screen_width / 2 or x > screen_width / 2 or y < -screen_height / 2 or y > screen_height / 2:
-            s = new_star()
+    return resized_frame
 
-        if s[3] < 255:
-            s[3] += 0.15
-
-        if s[3] > 256:
-            s[3] = 255
-
-        stars[i] = s
-
-        x = (s[0] * 256 / s[2]) + screen_width / 2
-        y = (s[1] * 256 / s[2]) + screen_height / 2
-
-        if current_time - last_color_change > color_change_time * 1000: # Это условие проверяет, прошло ли определенное количество времени (в миллисекундах) с момента последнего изменения цвета звезды. Если это условие выполняется, значит пришло время изменить цвет.
-            star_colors[i] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) # Здесь генерируется новый случайный цвет для звезды
-            last_color_change = current_time # После изменения цвета обновляется значение `last_color_change` до текущего времени, чтобы запомнить время последнего изменения цвета.
-
-        pygame.draw.circle(screen, star_colors[i], (x, y), 3)
-
-    pygame.display.flip()
-pygame.quit()
+def process_video_frame(model, frame, class_names, display_width):
+    frame_with_objects = detect_objects(model, frame, class_names, display_width)
+    rgb_frame = cv2.cvtColor(frame_with_objects, cv2.COLOR_BGR2RGB)
+    return rgb_frame
